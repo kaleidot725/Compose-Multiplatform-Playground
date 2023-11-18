9b910069539be164e3f2e0af587fd915a3268afd
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-11-18 16:27:03 +0900

    Add screen parameter

--- projects/Navigation/composeApp/src/commonMain/kotlin/common/screen/PreviewScreen.kt
+++ projects/Navigation/composeApp/src/commonMain/kotlin/common/screen/PreviewScreen.kt
@@ -2,9 +2,11 @@ package common.screen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import common.resource.Picture
 
 @Composable
 fun PreviewScreen(
+    picture: Picture,
     modifier: Modifier = Modifier
 ) {
 
